f079ec79f3137edd02d2eddad792eb365c6dc115
Selali Adobor <selali.adobor@gmail.com>
2020-10-18 14:10:13 -0400

    Fix SSE implementation

--- src/controllers/game_state.go
+++ src/controllers/game_state.go
@@ -2,11 +2,9 @@ package controllers
 
 import (
 	"github.com/SelaliAdobor/henchies-backend-go/src/schema"
-	"github.com/gin-contrib/sse"
 	"github.com/gin-gonic/gin"
 	"github.com/sirupsen/logrus"
 	"io"
-	"strconv"
 )
 
 func (env *Controllers) GetGameState(c *gin.Context) {
@@ -16,32 +14,18 @@ func (env *Controllers) GetGameState(c *gin.Context) {
 		WriteInvalidRequestResponse(c, err)
 		return
 	}
+	stateChan, err := env.GameRepository.SubscribeGameState(request.GameId, request.PlayerId, request.PlayerKey)
+	defer close(stateChan)
+	if err != nil {
+		logrus.Error("failed to subscribe to game state", err)
+		return
+	}
+
 	c.Stream(func(w io.Writer) bool {
-
-		stateChan, err := env.GameRepository.SubscribeGameState(request.GameId, request.PlayerId, request.PlayerKey)
-		defer close(stateChan)
-
-		if err != nil {
-			logrus.Error("Failed to set websocket upgrade", err)
-			return false
+		if state, ok := <-stateChan; ok {
+			c.SSEvent("game_state_changed", state)
+			return true
 		}
-
-		for {
-			eventId := 0
-
-			state := <-stateChan
-
-			err = sse.Encode(w, sse.Event{
-				Id:    strconv.Itoa(eventId),
-				Event: "game_state_changed",
-				Data:  state,
-			})
-
-			if err != nil {
-				logrus.Error("Failed to set websocket upgrade", err)
-				return true
-			}
-		}
+		return false
 	})
-
 }
